786153336562c147ff1af4d1720a9643b09c3e63
helahi3 <hamza@elahi.pk>
2018-09-25 12:27:12 -0400

    Added owners page and controller

---
+++ pet-clinic-web/src/main/java/com/hamza/springpetclinic/controller/OwnerController.java
@@ -0,0 +1,15 @@
+package com.hamza.springpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
+    public String listOwner(){
+
+        return "owners/index";
+    }
+
+}
