1a07af0598dc312461f81d66abf0d744e61ec95f
helahi3 <hamza@elahi.pk>
2018-09-27 14:34:00 -0400

    abstracted id implementation in map service

--- pet-clinic-web/src/main/java/com/hamza/springpetclinic/boostrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/hamza/springpetclinic/boostrap/DataLoader.java
@@ -22,28 +22,24 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Daniyal");
         owner1.setLastName("Rana");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Zain");
         owner2.setLastName("Ali");
 
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Owais");
         vet1.setLastName("Khan");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Safder");
         vet2.setLastName("Haider");
 
