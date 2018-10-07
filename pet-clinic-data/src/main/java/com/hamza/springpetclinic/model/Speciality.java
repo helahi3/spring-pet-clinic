dd52f3205a092bc76005a961644c5655861fc655
helahi3 <hamza@elahi.pk>
2018-10-07 13:04:26 -0400

    added speciality object

---
+++ pet-clinic-data/src/main/java/com/hamza/springpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.hamza.springpetclinic.model;
+
+public class Speciality {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
