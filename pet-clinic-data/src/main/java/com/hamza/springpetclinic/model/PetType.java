1ccf9df53a8a990bfae984421e79b33302fd19e5
helahi3 <hamza@elahi.pk>
2018-10-07 13:01:08 -0400

    added layouts to vet and owners page

--- pet-clinic-data/src/main/java/com/hamza/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/hamza/springpetclinic/model/PetType.java
@@ -1,4 +1,14 @@
 package com.hamza.springpetclinic.model;
 
 public class PetType extends BaseEntity {
+
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
